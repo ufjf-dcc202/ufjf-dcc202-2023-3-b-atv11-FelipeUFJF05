b5f6d0c89515c465239f49420639f98a76c7b346
Felipe Lima Nogueira <felipe.nogueira@estudante.ufjf.br>
2023-11-21 22:19:41 -0300

    cria 3 li

--- main.js
+++ main.js
@@ -7,3 +7,11 @@ const olItens = document.querySelector("#itens");
 const li = document.createElement("li");
 li.textContent = "Primeiro";
 olItens.appendChild(li);
+
+const li2 = document.createElement("li");
+li.textContent = "Segundo";
+olItens.appendChild(li2);
+
+const li3 = document.createElement("li");
+li.textContent = "Terceiro";
+olItens.appendChild(li3);
